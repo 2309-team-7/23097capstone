8cf0e14cbaadfcf686a729ef42d7b6988bc25384
VincentNguyen4 <vincent@VincentsOmen>
2023-11-30 12:42:07 -0600

    added db methods

--- src/server/db/comments.js
+++ src/server/db/comments.js
@@ -1,4 +1,5 @@
 const db = require('./client')
+const utils = require('./utils')
 
 const createComment = async({ reviewId, userId, content }) => {
   try {
@@ -13,8 +14,38 @@ const createComment = async({ reviewId, userId, content }) => {
   }
 }
 
+const updateComment = async(id, userId, fields ={}) => {
+
+  const setString = Object.keys(fields).map((key, index) => `'${key}'=$${index + 1}`).join(', ');
+  if ( setString === 0) {
+    return;
+  }
+  try {
+    const values = [...Object.values(fields), id, userId];
+    const { rows: [ comment ]} = await db.query(`
+    UPDATE comments SET ${setString}, "userId"=$${Object.keys(fields).length + 1}
+    WHERE id=$${Object.keys(fields).length +2}
+    RETURNING *;
+    `, values);
+    return comment;
+  } catch (err) {
+      throw err
+  }
+}
+
+const deleteComment = async(id) => {
+  try {
+    const { rows: [ comment ] } = await db.query(`
+    DELETE FROM comments WHERE id = ${id} RETURNING *;`);
+      return comment;
+  } catch (error) {
+      throw error
+  }
+}
 
 
 module.exports = {
   createComment,
+  updateComment,
+  deleteComment
 };
--- src/server/db/reviews.js
+++ src/server/db/reviews.js
@@ -1,4 +1,5 @@
 const db = require('./client')
+const utils = require('utils')
 
 const createReview = async({ itemId, userId, content, rating }) => {
   try {
@@ -12,8 +13,36 @@ const createReview = async({ itemId, userId, content, rating }) => {
       throw err;
   }
 }
+// PUT -  /api/reviews/:id
+const updateReview = async(id, fields = {}) => {
+  const setString = Object.keys(fields).map((key,index) => `'${key}'=$${index + 1}`).join(', ');
+  if (setString.length === 0) {
+    return;
+  }
+  try {
+    const { rows: [review] } = await db.query(`
+    UPDATE reviews SET ${setString} WHERE id=${id} RETURNING *
+   `, Object.values(fields));
+    return review;
+  } catch (err) {
+      throw err
+  }
+}
+
+// DELETE - /api/reviews/:id
+const deleteReview = async(id) => {
+  try {
+    const { rows: [review] } = await db.query(`
+      DELETE FROM reviews WHERE id=${id}`)
+      return review;
+  } catch (err) {
+      throw err
+  }
+}
 
 
 module.exports = {
   createReview,
+  updateReview,
+  deleteReview
 };
