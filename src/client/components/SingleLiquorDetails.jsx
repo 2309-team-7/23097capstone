ee862535c5415553670db35288ed19f8e054886c
Brian <hijoputo@Brians-MacBook-Air.local>
2023-12-06 12:00:36 -0800

    added three components

---
+++ src/client/components/SingleLiquorDetails.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import styles from "./SingleLiquor.module.css";
+
+export const API = "http://localhost:5432/2309-7-db/api";
+
+async function fetchLiquorById(item_id) {
+  const response = await Axios.get(`${API}/items/${item_id}`);
+  return response;
+}
+export function LiquorDetails({ token, item_id }) {
+  const [itemData, setLiquor] = useState();
+
+  useEffect(() => {
+    async function getAndSetLiquor() {
+      try {
+        const response = await fetchLiquorById(item_id);
+        setLiquor(response.data.item);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAndSetLiquor();
+  }, []);
+
+  if (!itemData) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <div className={styles.div}>
+      <img className={styles.img} src={itemData.imageUrl} />
+      <h1>{itemData.name}</h1>
+      <p className={styles.p}>{itemData.description}</p>
+      <p className={styles.p}>${itemData.price}</p>
+      <p className={styles.p}>ABV: {itemData.alcohol_content}</p>
+      <p className={styles.p}>Category: {itemData.category}</p>
+    </div>
+  );
+}
